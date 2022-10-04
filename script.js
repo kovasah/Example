146f75ee9a35e64ac76d4043230e66576ef5cbb0
kovasah <samkovacevic05@gmail.com>
2022-10-04 11:23:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-let fruits = ["Pomme", "Orange", "Fraise"];
+let myAlphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "$",
+  "*",
+  "/",
+  "-",
+  "+",
+];
 
-fruits.forEach((fruit) => {
-  console.log(fruit);
+function myAlphabetLength() {
+  console.log(myAlphabet.length) 
+}
+
+myAlphabetLength();
+
+let planets = [
+  "Terre","Mars","Venus","Mercure","Jupiter"
+]
+
+planets.forEach((planet) => {
+  console.log(planet);
 });
 
+let someDataTypes = []
