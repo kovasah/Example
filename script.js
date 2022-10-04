8ec58087edef975605cc3f5a5389c5113f87e45c
kovasah <samkovacevic05@gmail.com>
2022-10-04 15:59:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,4 +46,59 @@ planets.forEach((planet) => {
   console.log(planet);
 });
 
-let someDataTypes = []
+planets.forEach((planet) => {
+  console.log(planets.indexOf(planet));
+});
+
+let someDataTypes = [1,"coucou",true,undefined];
+
+someDataTypes.forEach((Element) => {
+  console.log(typeof Element)
+});
+
+someDataTypes.forEach((dataType) => {
+  console.log(someDataTypes.indexOf(dataType));
+});
+
+let emptyArray = [];
+
+console.log(emptyArray);
+
+let copyArray = emptyArray.slice();
+
+copyArray = [1,2,3,4];
+
+console.log(copyArray);
+
+let furnitures = ['Table', 'Chairs','Couch'];
+
+furnitures.forEach(element => {
+  console.log(element.split(''))
+});
+
+
+let values1 = ["Apple", 1, false, "2"];
+let values2 = [`5`, "Fries", 2 , true];
+let values3 = ["Mars", "Strawberry", 9];
+
+values1.splice(2, 2)
+values1.shift()
+values2.splice(0, 2)
+values2.pop()
+values3.splice(0, 2)
+
+console.log(values1)
+console.log(values2)
+console.log(values3)
+
+
+function isNumber(array) {
+  array.forEach((arrayElement) => {
+    if (typeof arrayElement === "number") {
+      console.log(arrayElement);
+    } 
+  });
+}
+
+
+
